b16b0c735a1c2b00b7526d791f844ad5c983aea3
AgusDAM <102875959+AgusDAM@users.noreply.github.com>
2023-03-26 17:25:12 +0200

    v5.0

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hola GitHub!!");
-        System.out.println("v4.0");
+        System.out.println("v5.0");
 
     }
     
