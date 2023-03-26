d30a5efef1ba07bc2f64693c83395eca11a6d378
Agus <agustin.alarcon@murciaeduca.es>
2023-03-26 16:52:26 +0200

    v1.0

---
+++ src/vista/Main.java
@@ -0,0 +1,12 @@
+package vista;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola GitHub!!");
+        System.out.println("v1.0");
+
+    }
+    
+}
